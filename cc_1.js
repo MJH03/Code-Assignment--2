407eebe6ec974c1f4ab9492858fbb11f0aa7c2ce
MJH03 <mjhandelsman@usf.edu>
2024-05-20 21:06:26 -0400

    Add functionality to display bill, tip, and total

--- cc_1.js
+++ cc_1.js
@@ -1,3 +1,4 @@
 //U68997808l
 let cost = 275
 let tip = cost <= 300 && cost >= 50 ? cost * 0.15 : cost * 0.2;
+console.log (`The bill was ${cost}, the tip was ${tip}, and the total is ${cost + tip}.`);
