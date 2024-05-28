b1a9ec4a20375376cb5b8c574ac734e81e26b01b
MJH03 <mjhandelsman@usf.edu>
2024-05-28 15:40:06 -0400

    Create Manager Class with Bonus Calculations

--- Coding Challenge #5/cc_5.js
+++ Coding Challenge #5/cc_5.js
@@ -3,13 +3,27 @@
 //Step 1: Employee Class
 class Employee {
     constructor(name, salary){
-        this.name = name
-        this.salary = salary
-                console.log(`Name: ${this.name}`)
-                console.log(`Monthly Salary: $${this.salary}`)               
+        this.name = name;
+        this.salary = salary;
+                console.log(`Name: ${this.name}`);
+                console.log(`Monthly Salary: $${this.salary}`);               
     }
     annualSalary(){
-        return this.salary * 12
+        return this.salary * 12;
     }
 
+}
+//Step 2: Management Subclass
+
+class Manager extends Employee {
+    constructor(name, salary, department)
+    {   super(name, salary);
+        this.department = department;
+                console.log(`Department: ${this.department}`);
+    }
+    annualSalary(){
+        const bonus = 0.15
+            console.log(`bonus: $${super.annualSalary() * bonus}`)
+return "Annual Salary: $" +((super.annualSalary() * bonus) + super.annualSalary())  
+}
 }
