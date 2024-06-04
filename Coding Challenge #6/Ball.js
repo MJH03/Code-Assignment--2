9efde13ec7b0e059876023ff391d80d7ef9c960a
MJH03 <mjhandelsman@usf.edu>
2024-06-04 13:50:33 -0400

    Create Ball constructor with intial properties

--- Coding Challenge #6/Ball.js
+++ Coding Challenge #6/Ball.js
@@ -1,1 +1,21 @@
 //U6899-7808
+
+// Step 2 Ball Constructor
+let canvas = document.querySelector("#bounce");
+let ctx = canvas.getContext("2d");
+
+let width = canvas.width;
+let height = canvas.height;
+
+let x=200, y=160
+
+class Ball {
+    constructor(x, y){
+    ctx.fillStyle = "red"
+    ctx.beginPath();
+    ctx.arc(x, y, 20, 0, Math.PI*2, false);
+    ctx.fill();
+    x += 0
+    y += 1
+}
+}
