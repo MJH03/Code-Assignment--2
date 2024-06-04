72021b9413ef5fb4f84b2b0cd5c367595ef0aa82
MJH03 <mjhandelsman@usf.edu>
2024-06-04 16:02:56 -0400

    Implement draw method for Ball class

--- Coding Challenge #6/Ball.js
+++ Coding Challenge #6/Ball.js
@@ -7,15 +7,22 @@ let ctx = canvas.getContext("2d");
 let width = canvas.width;
 let height = canvas.height;
 
-let x=200, y=160
-
 class Ball {
-    constructor(x, y){
-    ctx.fillStyle = "red"
-    ctx.beginPath();
-    ctx.arc(x, y, 20, 0, Math.PI*2, false);
-    ctx.fill();
-    x += 0
-    y += 1
-}
+    constructor(x, y, radius, color, dx, dy){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.dx = dx
+        this.dy = dy
+ 
+    } // Step 3 Draw Function
+    drawCircle(){
+        ctx.fillStyle = `${this.color}`
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI *2, false);
+        ctx.fill()
+        this.x += this.dx
+        this.y += this. dy
+    }
 }
