1a596790d8b5cedddfcc640edcdd404d5a01d650
MJH03 <mjhandelsman@usf.edu>
2024-06-14 01:45:17 -0400

    Implement interactive behaviors  with D3.js

--- Coding Challenge #9/script.js
+++ Coding Challenge #9/script.js
@@ -1,4 +1,16 @@
 //U6899-7808
-function main(){
 
+//Step 3
+//Implementing interactive activity
+function main(){
+    
+    d3.selectAll("div")
+        .on("mouseover", function(){
+                d3.select(this)
+                    .style("background-color", "firebrick")
+        })
+        .on("mouseout", function(){
+            d3.select(this)
+                .style("background-color", "seagreen")
+    })
 }
